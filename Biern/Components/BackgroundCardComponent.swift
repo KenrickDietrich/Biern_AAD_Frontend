7070a34a53bd2c972645133b1897f9f1433ead6e
Kenrick Dietrich <kenrick.dietrich@nedap.com>
2020-09-24 16:01:04 +0200

    loaded component in main view

---
+++ Biern/Components/BackgroundCardComponent.swift
@@ -0,0 +1,21 @@
+//
+//  BackgroundCardComponent.swift
+//  Biern
+//
+//  Created by Kenrick Dietrich on 24/09/2020.
+//  Copyright © 2020 AAD_Group_4. All rights reserved.
+//
+
+import SwiftUI
+
+struct BackgroundCardComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BackgroundCardComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundCardComponent()
+    }
+}
