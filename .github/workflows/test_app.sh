9c497de3ce3fc51c48e1b9640ec917850165f2ac
Kenrick Dietrich <kenrickdietrich@gmail.com>
2020-09-25 08:22:01 +0200

    Update test_app.sh

--- .github/workflows/test_app.sh
+++ .github/workflows/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace Biern.xcworkspace \
+xcodebuild -workspace project.xcworkspace \
             -scheme Biern\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
