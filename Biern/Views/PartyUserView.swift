36c1d33fbeb4003e5d9cb97931c0903e3e13da88
Kenrick Dietrich <kenrickdietrich@gmail.com>
2020-10-09 14:37:33 +0200

    Update Biern/Views/PartyUserView.swift

--- Biern/Views/PartyUserView.swift
+++ Biern/Views/PartyUserView.swift
@@ -13,7 +13,7 @@ struct PartyUserView: View {
                     HStack {
                         VStack {
                             Text("Username:")
-                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.bold)
                                 .padding(2)
                             Text("Current Room:")
                                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
